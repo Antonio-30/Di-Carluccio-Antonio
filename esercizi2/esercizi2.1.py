7dc2d417523a4d2f944a495f6add16d44d5cdb29
Antonio-30 <75388299+Antonio-30@users.noreply.github.com>
2021-03-10 16:51:20 +0100

    Add files via upload

---
+++ esercizi2/esercizi2.1.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+domanda1=int(input("inserisci 5 numeri interi"))
+domanda2=int(input("inserisci 5 numeri interi"))
+domanda3=int(input("inserisci 5 numeri interi"))
+domanda4=int(input("inserisci 5 numeri interi"))
+domanda5=int(input("inserisci 5 numeri interi"))
+
+array=[domanda1, domanda2, domanda3, domanda4, domanda5]
+
+array.lsort()
+
+array.reverse()
+
+print(array)
