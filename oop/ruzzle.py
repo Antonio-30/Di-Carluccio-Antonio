c914319b88ee2503fdb34c4b1431dc70ff430290
Antonio-30 <75388299+Antonio-30@users.noreply.github.com>
2021-11-17 23:00:15 +0100

    Add files via upload

--- oop/ruzzle.py
+++ oop/ruzzle.py
@@ -1,3 +1,4 @@
+from itertools import permutations
 class calcComb():
 
     def __init__(self, stringa):
@@ -12,23 +13,78 @@ class calcComb():
     def get_listStringa(self):
         return self.__listStringa
 
-    def setStringa(self):
-        try:
-            self.__stringa=str(self.__stringa)
+    def setStringa(self,str):
+        self.__N = len(stringa)
+        self.__stringa = str
+        self.__listStringa = list(stringa)
 
-        except ValueError:
-            print("errore, inserisci una parola)")
-       
+    def anagrammi(self):
+        lettere = list(parola)
+
+        permutazioni = list(permutations(lettere))
+
+        temp = ''
+
+        anagrammi = []
+
+        for i in permutazioni:
+
+            for carattere in i:
+                temp += carattere
+
+            anagrammi.append(temp)
+
+            temp = ''
+
+
+    def charRipetuti(self):
+
+        word = list(parola)
+        caratteriripetuti={}
+        nCaratteri = 0
+        count = 0
+
+        for i in word:
+            if (i in caratteriripetuti):
+                caratteriripetuti[str(i)] += 1
+        else:
+            caratteriripetuti[str(i)] = 1
+        for i in caratteriripetuti:
+            if caratteriripetuti[i]>1:
+                count+=1
+                nCaratteri += caratteriripetuti[i]
+               
     def combUtil(self):
-        f = open("C:/Users/Antonio/Desktop/coding/4E/words.italian.txt, 'r')
+        words = 'C:/Users/Antonio/Desktop/coding/4E/words.italian.txt'
+        f = open(words, 'r')
         for riga in f:
 
            p=f.readline()
 
            if self.__stringa in p:
-               print("è una parola italiana")
-           #else:
-               #print("non è una parola italiana")
+               return("è una parola italiana")
+           if parola == p[:-1]:
+               return("vero")
 
-casa=calcComb("casa")
-casa.combUtil()
+    def fattoriale(n):
+        if n==0:
+            return 1
+        else:
+            return n*fattoriale(n-1)
+
+
+    def coeffBinom(n, k):
+        x = len(parola)
+        y = int(input("Enter a value for y: "))
+        if y == 1 or y == x:
+           return(1)
+        if y > x:
+            return(0)
+        else:
+            a = fattoriale(x)
+            b = fattoriale(y)
+            div = a // (b*(x-y))
+        return(div)  
+
+parola= calcComb(str(input("inserisci una stringa: ")))
+parola.combUtil()
